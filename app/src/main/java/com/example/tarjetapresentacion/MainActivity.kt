41eea588270cb0e0e267fc113b8bfd88128f5af9
paperec <91558365+paperec@users.noreply.github.com>
2024-02-20 21:00:18 +0100

    ACABADA

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.tarjetapresentacion
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tarjetapresentacion.ui.theme.TarjetaPresentacionTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Tarjeta(
+                        "paula",
+                        "estudia",
+                        123,
+                        "paperec",
+                        "pape@uni.es")
                 }
             }
         }
@@ -30,17 +49,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Tarjeta(nombre:String,
+            cargo:String,
+            telefono:Int,
+            arroba:String,
+            correo:String,
+            modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(
+                top = 100.dp
+            )
+    ){
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(
+                    painter = painterResource(R.drawable.android_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .background(Color.Gray)
+                        .padding(
+                            start = 20.dp,
+                            end = 20.dp
+                        )
+                )
+                Text(
+                    text = nombre,
+                    fontSize = 70.sp
+                )
+                Text (
+                    text = cargo,
+                    fontSize = 40.sp
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .padding(
+                    top = 50.dp
+                ),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                var tlf = "+34 ${telefono}"
+                Text (
+                    text = tlf
+                )
+                var arr = "@$arroba"
+                Text (
+                    text = arr
+                )
+                Text (
+                    text = correo
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TarjetaPresentacionTheme {
-        Greeting("Android")
+        Tarjeta(
+            "paula",
+            "estudia",
+            123,
+            "paperec",
+            "pape@uni.es")
     }
 }
